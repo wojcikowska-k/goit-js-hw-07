7854b863bfd5d9102112a164a9ddbff35e12875a
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-03-28 19:57:46 +0200

    gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,38 +19,17 @@ for (const image of galleryItems) {
   galleryEl.insertAdjacentHTML("beforeend", listEl);
 
   galleryEl.addEventListener("click", (event) => event.preventDefault());
-
-  galleryEl.querySelector(".gallery__link").onclick = () => {
-    const instance = basicLightbox.create(`<img src=${image.original}>`);
-    instance.show();
-  };
 }
 
-// for (let i = 0; i < galleryItems.length; i++) {
-//   const listEl = `<div class="gallery__item">
-//     <a class="gallery__link" href=${galleryItems[i].original}>
-//       <img
-//        class="gallery__image"
-//        src=${galleryItems[i].preview}
-//         data-source=${galleryItems[i].original}
-//         alt=${galleryItems[i].description}
-//      />
-//     </a>
-//    </div>`;
-//   galleryEl.insertAdjacentHTML("beforeend", listEl);
+galleryEl.addEventListener("click", (event) => {
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source}>`
+  );
+  instance.show();
 
-//   galleryEl.addEventListener("click", (event) => event.preventDefault());
-
-//   galleryEl.querySelector(".gallery__link").onclick = () => {
-//     const instance = basicLightbox.create(
-//       `<img src=${galleryItems[i].original}>`
-//     );
-//     instance.show();
-//   };
-// }
-
-// document.addEventListener("keydown", (event) => {
-//   if (event.key === "Escape") {
-//     instance.close();
-//   }
-// });
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+});
