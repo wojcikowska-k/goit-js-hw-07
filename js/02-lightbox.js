3d8a1a8f216a0c3ddf5a5b61380deac9039ed575
Katarzyna Wojcikowska <wojcikowska.k@gmail.com>
2023-03-30 23:40:41 +0200

    task#2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+  const listEl = `<li class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+     <img class="gallery__image" src=${image.preview} alt=${image.description} />
+  </a>
+</li>`;
+  galleryEl.insertAdjacentHTML("beforeend", listEl);
+
+  galleryEl.addEventListener("click", (event) => event.preventDefault());
+}
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
